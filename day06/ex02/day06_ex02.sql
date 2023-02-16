d9ea55debd866284b0ef8eb3effad6bb3bb2184d
Morra Monarch <mmonarch@ge-n6.kzn.21-school.ru>
2023-02-16 11:58:25 +0300

    change day06_ex02

--- day06/ex02/day06_ex02.sql
+++ day06/ex02/day06_ex02.sql
@@ -1,8 +1,9 @@
-select
-    name, pizza_name, price,
-    round(price * (1 - discount/100)) as discount_price
-from person p
-join person_order po on p.id = po.person_id
-join menu m on m.id = po.menu_id
-join person_discounts pd on p.id = pd.person_id
+select p.name as name, pizza_name, price, (price - price * discount / 100)
+              as discount_price, p2.name as pizzeria_name
+from person_order po join menu m on m.id = po.menu_id
+                     join person p on p.id = po.person_id
+                     join pizzeria p2 on p2.id = m.pizzeria_id
+                     join person_discounts pd on p.id = pd.person_id and pd.pizzeria_id = p2.id
 order by name, pizza_name;
+
+
