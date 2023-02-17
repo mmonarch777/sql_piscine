b9c9a7ddfb911af062e7be8131af095dce581119
Morra Monarch <mmonarch@ge-n6.kzn.21-school.ru>
2023-02-17 12:45:42 +0300

    done

--- day07/ex02/day07_ex02.sql
+++ day07/ex02/day07_ex02.sql
@@ -4,13 +4,16 @@ with action_order as (select name, count(menu_id) as count, 'order' as action_ty
                         join pizzeria p on p.id = m.pizzeria_id
                group by
                    name
-               limit 3),
+               order by
+                   count desc
+                      limit 3),
     action_visit as (select name, count(person_id) as count, 'visit' as action_type
                      from person_visits pv join pizzeria p2 on p2.id = pv.pizzeria_id
                      group by name
+                     order by count desc
                      limit 3)
 
 select * from action_order
 union all
 select * from action_visit
-order by action_type, count desc ;
+order by action_type;
